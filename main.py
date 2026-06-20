45fdbd262eac0326e850c5cb3c5624d15f10f430
demon <dvor85@gmail.com>
2026-06-20 11:32:33 +0400

    Итог план/факт

--- main.py
+++ main.py
@@ -17,46 +17,65 @@ warnings.filterwarnings(
 
 
 class Summator:
-    def __init__(self, path: Path):
+    def __init__(self, path: Path, template: str):
         self.path = path
+        self.template = template
+        self.type = (
+            "факт" if "import-package-units-template" in self.template else "план"
+        )
 
-    @staticmethod
-    def read_file(filename: Path) -> pd.DataFrame:
-        df = pd.read_excel(filename, usecols="A,B,C,F").astype(
-            {
+    @property
+    def columns(self) -> dict[str, str]:
+        if self.type == "факт":
+            return {
                 "ШК товара": "string",
                 "Артикул товара": "string",
                 "Кол-во товаров": "Int64",
             }
-        )
+        else:
+            return {
+                "артикул": "string",
+                "имя (необязательно)": "string",
+                "количество": "Int64",
+            }
+
+    def read_file(self, filename: Path) -> pd.DataFrame:
+        df = pd.read_excel(filename, usecols="A,B,C").astype(self.columns)
         return df
 
     @staticmethod
     def format(worksheet):
-        worksheet.column_dimensions["A"].width = 16
-        worksheet.column_dimensions["B"].width = 35
+        worksheet.column_dimensions["A"].width = 20
+        worksheet.column_dimensions["B"].width = 50
         worksheet.column_dimensions["C"].width = 6
 
     def gen_group_version(self) -> None:
         dfs = self.read_dir()
-        result = (
-            pd.concat(dfs, ignore_index=True)
-            .groupby(["ШК товара", "Артикул товара"], as_index=False, sort=False)[
-                "Кол-во товаров"
-            ]
-            .sum()
-        ).query("`Кол-во товаров` > 0")
-        with pd.ExcelWriter(self.path / "Итог.xlsx") as writer:
-            result.to_excel(
-                writer,
-                index=False,
-                sheet_name="Сводная",
-            )
-            worksheet = writer.sheets["Сводная"]
-            self.format(worksheet)
+        gen_file = self.path / f"Итог {self.type}.xlsx"
+        try:
+            sum_col = [k for k, v in self.columns.items() if v == "Int64"][0]
+            result = (
+                pd.concat(dfs, ignore_index=True)
+                .groupby(
+                    [k for k, v in self.columns.items() if v == "string"],
+                    as_index=False,
+                    sort=False,
+                )[sum_col]
+                .sum()
+            ).query(f"`{sum_col}` > 0")
+            with pd.ExcelWriter(gen_file) as writer:
+                result.to_excel(
+                    writer,
+                    index=False,
+                    sheet_name="Сводная",
+                )
+                worksheet = writer.sheets["Сводная"]
+                self.format(worksheet)
+        except Exception as e:
+            logger.warning(f"Нет файлов сооветствующих шаблону '{self.template}': {e}")
 
     def read_dir(self) -> Generator[pd.DataFrame]:
-        for f in self.path.rglob("import-package-units-template*.xlsx"):
+        for f in self.path.rglob(self.template):
             yield self.read_file(f)
 
 
@@ -84,28 +103,38 @@ class PrintPakages:
         worksheet.column_dimensions["D"].width = 18
 
     def gen_print_version_in_sheet(self) -> None:
-        with pd.ExcelWriter(self.path / "Грузоместа.xlsx") as writer:
-            startrow = 0
-            for f in self.path.rglob("import-package-units-template*.xlsx"):
-                df = self.read_file(f)
-                city = f.parent.name.capitalize()
-                # Пишем строку-разделитель (займёт всю ширину таблицы)
-                sep_row = pd.DataFrame([[city] + [""] * (len(df.columns) - 1)])
-                sep_row.to_excel(
-                    writer,
-                    sheet_name="Сводная",
-                    startrow=startrow,
-                    header=False,
-                    index=False,
-                )
-                startrow += 1
+        gen_file = self.path / "Грузоместа.xlsx"
+        try:
+            with pd.ExcelWriter(gen_file) as writer:
+                startrow = 0
+                for f in self.path.rglob("import-package-units-template*.xlsx"):
+                    city = f.parent.name.capitalize()
+                    try:
+                        df = self.read_file(f)
+                        # Пишем строку-разделитель (займёт всю ширину таблицы)
+                        sep_row = pd.DataFrame([[city] + [""] * (len(df.columns) - 1)])
+                        sep_row.to_excel(
+                            writer,
+                            sheet_name="Сводная",
+                            startrow=startrow,
+                            header=False,
+                            index=False,
+                        )
+                        startrow += 1
 
-                df.to_excel(
-                    writer, index=False, startrow=startrow, sheet_name="Сводная"
-                )
-                startrow += len(df) + 1
-                worksheet = writer.sheets["Сводная"]
-                self.format(worksheet)
+                        df.to_excel(
+                            writer, index=False, startrow=startrow, sheet_name="Сводная"
+                        )
+                        startrow += len(df) + 1
+                        worksheet = writer.sheets["Сводная"]
+                        self.format(worksheet)
+                    except Exception as e:
+                        logger.warning(f"Ошибка при обработке города {city}: {e}")
+        except Exception as e:
+            logger.warning(
+                f"Нет файлов сооветствующих шаблону 'import-package-units-template*.xlsx': {e}"
+            )
+            gen_file.unlink(missing_ok=True)
 
     def gen_print_version_by_sheets(self) -> None:
         with pd.ExcelWriter(self.path / "Грузоместа.xlsx") as writer:
@@ -201,8 +230,11 @@ if __name__ == "__main__":
     collector = PackageCollector(root_path)
     collector.run()
 
-    summator = Summator(root_path)
-    summator.gen_group_version()
+    fact_summator = Summator(root_path, "import-package-units-template*.xlsx")
+    fact_summator.gen_group_version()
+
+    plan_summator = Summator(root_path, "Шаблон поставки товаров*.xlsx")
+    plan_summator.gen_group_version()
 
     print_pakages = PrintPakages(root_path)
     print_pakages.gen_print_version_in_sheet()
